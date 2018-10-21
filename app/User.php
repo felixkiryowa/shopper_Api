cefbd3101e3cf6705c627b4fdd88fd0587b7d897
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-10-21 19:22:54 -0400

    Add Auth logic into the controller

--- app/User.php
+++ app/User.php
@@ -10,6 +10,24 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
     // Rest omitted for brevity
 
     /**
